78b8edd7d60e83f6d7eaf132c548dd3cd8db2afb
LinuxUser255 <atnl@protonmail.com>
2025-10-09 00:24:52 -0400

    created a YouTube link sanitizer CLI tool

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+/*
+ URL link sanitizer
+ Remove the source identifiers from the URL.
+ Example:
+ https://youtu.be/zfb1y8yn8QI?si=k5puuw5JPpjnzmMm
+ Remove everything after the ? symbol.
+*/
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+
+struct Args {
+    #[arg(short, long)]
+    url: String,
+}
+
+fn sanitize_url(url: &str) -> String {
+    if let Some(q_pos) = url.find('?') {
+        url[..q_pos].to_string()
+    } else {
+        url.to_string()
+    }
+}
+
+fn main() {
+    let args = Args::parse();
+    let sanitized_url = sanitize_url(&args.url);
+    println!("Sanitized URL: {}", sanitized_url);
+}
+
